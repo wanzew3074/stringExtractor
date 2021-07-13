93e8512867705673724e71b3b3ea3a10eb9a4331
wanzew <wanzew@gmail.com>
2021-07-13 18:45:23 +0800

    added function

--- read.py
+++ read.py
@@ -1,9 +1,34 @@
+def read():
+    systemInfo_file = open("readTest.txt", "r")
+
+    for systemInfo in systemInfo_file.readlines():
+        print(systemInfo)
+
+    systemInfo_file.close()
 
 
-systemInfo_file = open("test.txt", "r")
 
-for systemInfo in systemInfo_file.readlines():
-    print(systemInfo)
+def write():
+    systemInfo_file = open("writeTest.txt", "a")
 
- 
-systemInfo_file.close()
+    systemInfo_file.write("\nTEST DATA2")
+
+    systemInfo_file.close()
+
+
+
+
+print("Please select operation -\n" \
+        "1. Read\n" \
+        "2. Write\n")
+
+select = int(input("Select operations form 1, 2:"))
+
+if select == 1:
+    read()
+
+elif select == 2:
+    write()
+
+else:
+    print("Invalid input")
---
+++ write.py
@@ -0,0 +1,5 @@
+systemInfo_file = open("writeTest.txt", "w")
+
+systemInfo_file .write("\nTEST DATA")
+
+systemInfo_file.close()
